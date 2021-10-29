52b77004d9349ffd0835fcb2f9c8c9376bcbf162
tiago <tiago.rainho@ua.pt>
2021-10-29 17:00:51 +0100

    remove all, remain dataset

--- src/inverted_index.test.py
+++
@@ -1,9 +0,0 @@
-from inverted_index import InvertedIndex
-
-index = InvertedIndex()
-index.add("test", 2)
-index.add("test", 1)
-index.add("another", 1)
-
-assert index.search("test") == [2, 1]
-assert index.search("another") == [1]
