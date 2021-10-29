4620bbd6ff3c0fe938f3d72a7abd919f341bd557
Vasco Sousa <myemailvasco@gmail.com>
2021-10-29 18:40:24 +0100

    Parser core finished and testing too

---
+++ test/parser_test.py
@@ -0,0 +1,12 @@
+from src.parser import Parser
+
+
+def test_parse():
+    parser = Parser("datasets/digital_video_games.tsv",
+                    "review_id", ["review_headline", "review_body"])
+    text = parser.parse("\t")
+
+    assert text != ""
+    assert "RSH1OZ87OYK92" in text
+    assert "R1JEEW4C6R89BA" in text
+    assert "A slight improvement from last year." in text
