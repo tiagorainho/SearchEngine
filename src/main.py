502dbaf6907ecbebfab1ae8adf8686d38d58a0c0
tiago <tiago.rainho@ua.pt>
2021-11-17 10:14:56 +0000

    add initial light searcher

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 
+from io import TextIOWrapper
 from models.spimi import Spimi
 from models.index import InvertedIndex
 from models.posting import PostingType
@@ -19,7 +20,7 @@ def remove_blocks(block_dir:str):
         os.remove(f)
 
 
-def main():
+def generate_index() -> InvertedIndex:
     # remove old blocks for better understanding of the blocks folder
     remove_blocks(os.path.join(os.path.dirname(__file__), f'../{BLOCK_DIR}'))
 
@@ -28,16 +29,13 @@ def main():
     parsed_text = parser.parse('\t')
     indexer = Spimi(max_ram_usage=95, max_block_size=50000, auxiliary_dir=f'../../{BLOCK_DIR}', posting_type=PostingType.FREQUENCY)
 
-    
     print("Creating documents and indexing")
     start = time.time()
-    i = 0
-    for doc_id in sorted([key for key in parsed_text.keys()]):
+    for i,doc_id in enumerate(sorted([key for key in parsed_text.keys()])):
         tokens = tokenizer.tokenize(parsed_text[doc_id])
         indexer.add_document(doc_id = i, tokens=tokens)
-        i += 1
-
-    
+        if i >= 10000:
+            break
     print(f"time to create blocks: {time.time()-start}")
 
 
@@ -45,9 +43,27 @@ def main():
     start = time.time()
     index:InvertedIndex = indexer.construct_index(f'../../{OUTPUT_INDEX}')
     print(f"time to create the main index: {time.time()-start}")
-    # print(index)
+    return index
+
+
+
+def main():
+    index:InvertedIndex = generate_index()
+    while True:
+        query = input("Query: ")
+        documents = index.light_search(query.split(' '))
+        print(documents)
+    
+    
 
 
 if __name__ == '__main__':
     #cProfile.run('main()')
-    main()
+    #main()
+    f:TextIOWrapper = open("src/a.txt", "r")
+
+    f.seek(0,2)
+    print(f.tell())
+
+
+    f.close()
